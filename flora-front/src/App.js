afd80d52347b4b0450e1f8529f2cba1a2346e88f
antonioarieiro <sfgfffs@gmail.com>
2021-10-17 16:07:53 -0300

    create routes

--- flora-front/src/App.js
+++ flora-front/src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Main from './middlewares/Main';
+import Home from './pages/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
 import { SubstrateContextProvider } from './substrate-lib';
 export default function App () {
   return (
     <SubstrateContextProvider>
-        <Main />
+     
+      <Router>
+        <Switch>
+        <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/create" exact>
+            <Main />
+          </Route>
+          <Route path="/404" exact>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
     </SubstrateContextProvider>
   );
 }

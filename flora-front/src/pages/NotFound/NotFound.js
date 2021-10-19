fe620fe8deb8cc62364671160227f4981dcc581f
antonioarieiro <sfgfffs@gmail.com>
2021-10-19 14:07:59 -0300

    in progress page

--- flora-front/src/pages/NotFound/NotFound.js
+++ flora-front/src/pages/NotFound/NotFound.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './notfoun.scss';
-export default function NotFound() {
-    return(
+export default function NotFound () {
+  return (
     <main className="main">
-    <nav class="shelf">
-  <a class="book home-page">Home page</a>
-  <a class="book about-us">About us</a>
-  <a class="book contact">Contact</a>
-  <a class="book faq">F.A.Q.</a>
-  
-  <span class="book not-found"></span>
- 
-  <span class="door left"></span>
-  <span class="door right"></span>
-</nav>
-<h1 className="title">coming soon</h1>
-<p></p>
+      <nav class="shelf">
+        <Link class="book home-page">Home page</Link>
+        <Link class="book about-us">About us</Link>
+        <Link class="book contact">Contact</Link>
+        <Link class="book faq">F.A.Q.</Link>
 
-</main>
-);
+        <span class="book not-found"></span>
+
+        <span class="door left"></span>
+        <span class="door right"></span>
+      </nav>
+      <h1 className="title">coming soon</h1>
+      <p></p>
+
+    </main>
+  );
 }

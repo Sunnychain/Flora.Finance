dd47c49b00481d22022b1a59f889c7ddb46b68a4
antonioarieiro <sfgfffs@gmail.com>
2021-10-19 14:08:34 -0300

    home page

--- flora-front/src/pages/Home/Home.js
+++ flora-front/src/pages/Home/Home.js
@@ -1,195 +1,192 @@
-import React, { useEffect,useState } from 'react';
-import { MdCollectionsBookmark } from "react-icons/md";
-import { RiHandHeartLine } from "react-icons/ri";
-import { GiBuyCard } from "react-icons/gi";
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { MdCollectionsBookmark } from 'react-icons/md';
+import { RiHandHeartLine } from 'react-icons/ri';
+import { GiBuyCard } from 'react-icons/gi';
 import Content from '../../components/Content/Content';
+import Wallet from '../../wallet/main';
 import $ from 'jquery';
 import './Home.scss';
 
 export default function Home () {
-  
-	function renderSizer () {
-    $(".sidebar-dropdown > a").click(function() {
-			$(".sidebar-submenu").slideUp(200);
-			if (
-			  $(this)
-				.parent()
-				.hasClass("active")
-			) {
-			  $(".sidebar-dropdown").removeClass("active");
-			  $(this)
-				.parent()
-				.removeClass("active");
-			} else {
-			  $(".sidebar-dropdown").removeClass("active");
-			  $(this)
-				.next(".sidebar-submenu")
-				.slideDown(200);
-			  $(this)
-				.parent()
-				.addClass("active");
-			}
-		  });
-		  
-		  $("#close-sidebar").click(function() {
-			$(".page-wrapper").removeClass("toggled");
-		  });
-		  $("#show-sidebar").click(function() {
-			$(".page-wrapper").addClass("toggled");
-		  });
-      console.log('montado')
+  function renderSizer () {
+    $('.sidebar-dropdown > a').click(function () {
+      $('.sidebar-submenu').slideUp(200);
+      if (
+        $(this)
+          .parent()
+          .hasClass('active')
+      ) {
+        $('.sidebar-dropdown').removeClass('active');
+        $(this)
+          .parent()
+          .removeClass('active');
+      } else {
+        $('.sidebar-dropdown').removeClass('active');
+        $(this)
+          .next('.sidebar-submenu')
+          .slideDown(200);
+        $(this)
+          .parent()
+          .addClass('active');
+      }
+    });
+    $('#close-sidebar').click(function () {
+      $('.page-wrapper').removeClass('toggled');
+    });
+    $('#show-sidebar').click(function () {
+      $('.page-wrapper').addClass('toggled');
+    });
   }
   useEffect(() => {
     renderSizer();
-  },[])
+  }, []);
+  return (
+    <main>
 
-  return(
-   <main>
-    <div className="page-wrapper chiller-theme toggled">
-      <a id="show-sidebar" className="btn btn-sm btn-dark" href="#">
-        <i className="fas fa-bars"></i>
-      </a>
-      <nav id="sidebar" className="sidebar-wrapper">
-        <div className="sidebar-content">
-          <div className="sidebar-brand">
-            <a href="#">Flora.Finance</a>
-            <div id="close-sidebar">
-              <i className="fas fa-times"></i>
-            </div>
-          </div>
-          <div className="sidebar-search">
-          <div>
-            <div className="input-group">
-              <input type="text" className="form-control search-menu" placeholder="Search..." />
-              <div className="input-group-append">
-                <span className="input-group-text">
-                  <i className="fa fa-search" aria-hidden="true"></i>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="sidebar-menu">
-          <ul>
-            <li className="header-menu">
-              <span>Actions</span>
+      <div className="accountHeader">
+        <nav>
+          <ul className="nav justify-content-end">
+            <li className="nav-item">
+              <Link className="nav-link active" to="#">Active</Link>
             </li>
-            <li>
-              <a href="#">
-                <i className="fa fa-home"></i>
-                <span>Home</span>
-              </a>
+            <li className="nav-item">
+              <Link className="nav-link" to="#">Link</Link>
             </li>
-            <li className="sidebar-dropdown">
-              <a href="#">
-                <i className="fa fa-chart-line"></i>
-                <span>Trade</span>
-              </a>
-              <div className="sidebar-submenu">
-                <ul>
-                  <li>
-                    <a href="#">Liquidity</a>
-                  </li>
-                  <li>
-                    <a href="#">exchange</a>
-                  </li>
-                </ul>
-              </div>
-            
-              <a href="#">
-                <i className="fa fa-tractor"></i>
-                <span>Farms</span>
-              </a>
+            <li className="nav-item">
+              <Link className="nav-link" to="#">Link</Link>
             </li>
-            <li>
-              <a href="#">
-                <i className="fa fa-calendar"></i>
-                <span>Staking</span>
-              </a>
-            </li>
-            
-            <li className="sidebar-dropdown">
-              <a href="#">
-                <i className="fa "><MdCollectionsBookmark/></i>
-                <span>Collections</span>
-              </a>
-              <div className="sidebar-submenu">
-                <ul>
-                  <li>
-                    <a href="#">All Collections</a>
-                  </li>
-                  <li>
-                    <a href="#">Create Collection</a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            
-            <li className="sidebar-dropdown">
-              <a href="#">
-                <i className="fa "><GiBuyCard/></i>
-                <span>Games</span>
-              </a>
-              <div className="sidebar-submenu">
-                <ul>
-                  <li>
-                    <a href="#">Card Game</a>
-                  </li>
-                </ul>
-              </div>
-              <li>
-              <a href="#">
-                <i className="fa fa-hearth"><RiHandHeartLine /></i>
-                <span>Donations</span>
-              </a>
-            </li>
-            </li>
-            <li className="header-menu">
-              <span>Extra</span>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa fa-book"></i>
-                <span>My NFTree</span>
-                <span className="badge badge-pill badge-primary">Beta</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa fa-calendar"></i>
-                <span>My Tokens</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa fa-folder"></i>
-                <span>Auctions</span>
-              </a>
+            <li className="nav-item">
+              <Wallet />
             </li>
           </ul>
-        </div>
+        </nav>
       </div>
-      <div className="sidebar-footer">
-        <a href="#">
-          <i className="fa fa-bell"></i>
-          <span className="badge badge-pill badge-warning notification"></span>
-        </a>
-        <a href="#">
-          <i className="fa fa-envelope"></i>
-          <span className="badge badge-pill badge-success notification"></span>
-        </a>
-        <a href="#">
-          <i className="fa fa-cog"></i>
-          <span className="badge-sonar"></span>
-        </a>
-        <a href="#">
-          <i className="fa fa-power-off"></i>
-        </a>
+      <div className='page-wrapper chiller-theme toggled'>
+        <Link id='show-sidebar' className='btn btn-sm btn-dark' to='#'>
+          <i className='fas fa-bars'></i>
+        </Link>
+        <nav id='sidebar' className='sidebar-wrapper'>
+          <div className='sidebar-content'>
+            <div className='sidebar-brand'>
+              <Link to='#'>Flora.Finance</Link>
+              <div id='close-sidebar'>
+                <i className='fas fa-times'></i>
+              </div>
+            </div>
+            <div className='sidebar-search'>
+
+              <div className='input-group'>
+                <input type='text' className='form-control search-menu' placeholder='Search...' />
+                <div className='input-group-append'>
+                  <span className='input-group-text'>
+                    <i className='fa fa-search' aria-hidden='true'></i>
+                  </span>
+                </div>
+              </div>
+
+            </div>
+            <div className='sidebar-menu'>
+              <ul>
+                <li className='header-menu'>
+                  <span>General</span>
+                </li>
+                <li>
+                  <Link to='#'>
+                    <i className='fa fa-home'></i>
+                    <span>Home</span>
+                  </Link>
+                </li>
+                <li className='sidebar-dropdown'>
+                  <Link to='#'>
+                    <i className='fa fa-chart-line'></i>
+                    <span>Trade</span>
+                  </Link>
+                  <div className='sidebar-submenu'>
+                    <ul>
+                      <li>
+                        <Link to='#'>Liquidity</Link>
+                      </li>
+                      <li>
+                        <Link to='#'>exchange</Link>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+                <li>
+                  <Link to='#'>
+                    <i className='fa fa-tractor'></i>
+                    <span>Farms</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='#'>
+                    <i className='fa fa-calendar'></i>
+                    <span>Staking</span>
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to='#'>
+                    <i className='fa '><MdCollectionsBookmark /></i>
+                    <span>NFTS</span>
+                  </Link>
+                </li>
+
+                <li className='sidebar-dropdown'>
+                  <Link to='#'>
+                    <i className='fa '><GiBuyCard /></i>
+                    <span>Games</span>
+                  </Link>
+                  <div className='sidebar-submenu'>
+                    <ul>
+                      <li>
+                        <Link to='#'>Card Game</Link>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+                <li>
+                  <Link to='#'>
+                    <i className='fa fa-hearth'><RiHandHeartLine /></i>
+                    <span>Donations</span>
+                  </Link>
+                </li>
+
+                <li className='header-menu'>
+                  <span>Info</span>
+                </li>
+                <li>
+                  <Link to='#'>
+                    <i className='fa fa-book'></i>
+                    <span>Project Info</span>
+                    <span className='badge badge-pill badge-primary'>Beta</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='#'>
+                    <i className='fa fa-folder'></i>
+                    <span>WhitePaper</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='#'>
+                    <i className='fa fa-folder'></i>
+                    <span>contributors</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {
+            window.location.pathname === '/' ? '' : ''
+          }
+
+        </nav>
+        <Content />
       </div>
-    </nav>
-     <Content />
-    </div>
-  </main>
+    </main>
+
   );
 }
-

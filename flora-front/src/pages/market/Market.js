b478d35a883bc2c906d9b4d0b1d291e5d33eb6ea
antonioarieiro <sfgfffs@gmail.com>
2021-10-24 02:57:13 -0300

    create page market and rout && render wallet address

--- flora-front/src/pages/market/Market.js
+++ flora-front/src/pages/market/Market.js
@@ -1,28 +1,91 @@
-import React, { useState } from 'react';
-import Menu from '../Home/Home';
-import { useSubstrate } from '../../substrate-lib';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Wallet from '../../wallet/main';
+import logo from '../../images/flora.svg';
+import example from '../../images/example.jpeg';
+import CreateAuction from '../../components/createAuction/NewAuction';
+
+import './market.scss';
 
 function Market () {
-  const { api } = useSubstrate();
-  const [data, setData] = useState({});
-
-  async function dale () {
-    try {
-      setData(await api.query.tokenNonFungible.collections(0));
-      const dale = data.toJSON();
-      console.log(dale);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
   return (
-    <main>
-      <Menu />
-  <div>
-    <h1 style={{ textAlign: 'center', marginTop: '-350px' }}>IN DEV</h1>
-  </div>
-  </main>
+    <div id="mainWrapper">
+      <header>
+        <div id="logo"><img src={logo} alt="logo"/></div>
+        <div id="headerLinks"><Wallet /></div>
+      </header>
+      <section id="offer">
+        <div className="semi"></div>
+        <h2>All our nfts represent real trees</h2>
+        <h4>you can buy nfts according to your rarities</h4>
+      </section>
+      <div id="content">
+        <section className="sidebar">
+
+          <input type="text" id="search" placeholder="search" />
+          <div id="menubar">
+            <nav className="menu">
+              Types
+              <br />
+              <h2>Rare</h2>
+              <hr />
+              <ul>
+                <li><Link to='#' title="Link">Ruis&nbsp;&nbsp;</Link></li>
+                <li><Link to='#' title="Link">Fearn</Link></li>
+                <li><Link to='#' title="Link">Paw</Link></li>
+              </ul>
+            </nav>
+            <nav className="menu">
+              <h2>Ultra Rare</h2>
+              <hr />
+              <ul>
+
+                <li><Link to='#' title="Link">Coll</Link></li>
+                <li><Link to='#' title="Link">Ngetal</Link></li>
+                <li><Link to='#' title="Link">Quert</Link></li>
+                <li className="notimp"></li>
+              </ul>
+            </nav>
+            <nav className="menu">
+              <h2>Legendary </h2>
+              <hr />
+              <ul>
+                <li><Link to='#' title="Link">Uath</Link></li>
+                <li><Link to='#' title="Link">Nion</Link></li>
+                <li><Link to='#' title="Link">Tinned</Link></li>
+              </ul>
+            </nav>
+          </div>
+        </section>
+        <section className="mainContent">
+          <div className="productRow">
+            <article className="productInfo" style={{ width: 'auto' }}>
+              <div><img alt="sample" src={example} /></div>
+              <br/> <hr/>
+              <p className="productContent">type </p>
+              <p className="productContent">Metadata </p>
+              <button type="button" name="button" defaultValue="Buy" className="buyButton"><b>$50</b></button>
+            </article>
+
+          </div>
+        </section>
+      </div>
+      <br />
+      <footer>
+        <div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius sem neque. Integer ornare.</p>
+        </div>
+        <div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam varius sem neque. Integer ornare.</p>
+        </div>
+        <div className="footerlinks">
+          <p><Link to='#' title="Link">Link to='#' 1 </Link></p>
+          <p><Link to='#' title="Link">Link to='#' 2</Link></p>
+          <p><Link to='#' title="Link">Link to='#' 3</Link></p>
+        </div>
+      </footer>
+    </div>
+
   );
 }
 
--- flora-front/src/wallet/wallet.js
+++ flora-front/src/wallet/wallet.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import { injected } from './connector';
-import { Button } from 'semantic-ui-react';
+import './wallet.scss';
 
 export default function Wallet () {
   const { active, account, library, activate, deactivate } = useWeb3React();
@@ -21,22 +21,26 @@ export default function Wallet () {
     }
   }
 
+  useEffect(() => {
+    connect();
+  }, []);
+
   return (
     <main>
-      <div style={ { marginTop: '5px', textAlign: 'center' } }>
+      <div>
         {
           active
             ? ''
-            : <Button positive onClick={ connect }>Connect wallet </Button>
+            : <button className="button" onClick={ connect }>Connect wallet </button>
         }
         {
           account
-            ? <span style={ { margin: '25px' } }> <b>Connected with</b> { account } { console.log('conta', library)}</span>
+            ? <span> <b>Connected with</b> { account } { console.log('conta', library)}</span>
             : ''
         }
         {
           account
-            ? <Button positive onClick={ disconnect } >Disconnect </Button>
+            ? <button className="button" onClick={ disconnect } >Disconnect </button>
             : ''
         }
       </div>

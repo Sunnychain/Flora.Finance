23ab5f308bc312c65da704d5eae8b7bbc32622c1
antonioarieiro <sfgfffs@gmail.com>
2021-10-25 00:08:57 -0300

    contact form

--- flora-front/src/pages/Contact/Contact.js
+++ flora-front/src/pages/Contact/Contact.js
@@ -1,34 +1,75 @@
 import React from 'react';
-
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import './Contact.scss';
 function Contact () {
   return (
-        <main className="main-contact">
-            <div class="container-content">
-                <form id="contact" action="" method="post">
-                    <h3>Colorlib Contact Form</h3>
-                    <h4>Contact us for custom quote</h4>
-                    <fieldset>
-                        <input placeholder="Your name" type="text" tabindex="1" required autofocus />
-                    </fieldset>
-                    <fieldset>
-                        <input placeholder="Your Email Address" type="email" tabindex="2" required />
-                    </fieldset>
-                    <fieldset>
-                        <input placeholder="Your Phone Number (optional)" type="tel" tabindex="3" required />
-                    </fieldset>
-                    <fieldset>
-                        <input placeholder="Your Web Site (optional)" type="url" tabindex="4" required />
-                    </fieldset>
-                    <fieldset>
-                        <textarea placeholder="Type your message here...." tabindex="5" required></textarea>
-                    </fieldset>
-                    <fieldset>
-                        <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-                    </fieldset>
-                    <p class="copyright">Designed by <a href="https://colorlib.com" target="_blank" title="Colorlib" rel="noreferrer">Colorlib</a></p>
-                </form>
-            </div>
+        <main>
+            <Nav defaultActiveKey="/home" as="ul">
+                <Nav.Item as="li">
+                    <Nav.Link href="/">Home</Nav.Link>
+                </Nav.Item>
+
+            </Nav>
+            <main className="mainContact">
+                <Form className="flora_form">
+                    <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formGridEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Email" />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="name" placeholder="Name" />
+                        </Form.Group>
+                    </Row>
+
+                    <Form.Group className="mb-3" controlId="formGridAddress1">
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control placeholder="1234 Main St" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formGridAddress2">
+                        <Form.Label>Address 2</Form.Label>
+                        <Form.Control placeholder="Apartment, studio, or floor" />
+                    </Form.Group>
+
+                    <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formGridCity">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridCity">
+                            <Form.Label>State</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridZip">
+                            <Form.Label>Zip</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+                    </Row>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>You Mensage</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" id="formGridCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </main>
         </main>
+
   );
 }
 
